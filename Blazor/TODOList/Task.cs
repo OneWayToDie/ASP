ee192f637b05e0aaf1c74eb9672983a304123c0a
Danila <Danila@DESKTOP-44LP0PV>
2026-09-02 21:39:27 +0300

    TODO in progress

---
+++ Blazor/TODOList/Task.cs
@@ -0,0 +1,17 @@
+﻿namespace TODOList
+{
+	public class Task
+	{
+		public string Description { get; set; }
+		public bool DONE { get; set; }
+
+		public override bool Equals(object? other)
+		{
+			return this.Description.Equals
+				(
+					(other as TODOList.Task).Description,
+					StringComparison.OrdinalIgnoreCase
+				);
+		}
+	}
+}
