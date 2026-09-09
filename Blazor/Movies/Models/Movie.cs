f0e99fa5b782877d5cdb4b35fc01c972f98bffe8
Danila <Danila@DESKTOP-44LP0PV>
2026-09-09 19:41:56 +0300

    Poster in EDIT & Details DONE

--- Blazor/Movies/Models/Movie.cs
+++ Blazor/Movies/Models/Movie.cs
@@ -5,6 +5,10 @@ namespace Movies.Models
 	public class Movie
 	{
 		public int Id { get; set; }
+
+		[Required]
+		[StringLength(50)]
+		[RangeAttribute(typeof(DateOnly), "1895-12-28", "9999-12-31")]
 		public string Title { get; set; }
 		public DateOnly ReleaseDate { get; set; }
 		public string Genre { get; set; }
