a7d5f0f0132e9231aaa02a61925fc50d88cc0368
Danila <Danila@DESKTOP-44LP0PV>
2026-09-09 18:25:40 +0300

    pu-pu pu... konec parbI

--- Blazor/Movies/Migrations/20260907174156_mssql.local_migration_484.Designer.cs
+++ Blazor/Movies/Migrations/20260907174156_mssql.local_migration_484.Designer.cs
@@ -11,8 +11,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Movies.Migrations
 {
     [DbContext(typeof(MoviesContext))]
-    [Migration("20260907160859_mssql.local_migration_754")]
-    partial class mssqllocal_migration_754
+    [Migration("20260907174156_mssql.local_migration_484")]
+    partial class mssqllocal_migration_484
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
@@ -36,10 +36,18 @@ namespace Movies.Migrations
                         .IsRequired()
                         .HasColumnType("nvarchar(max)");
 
+                    b.Property<string>("Poster")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(max)");
+
                     b.Property<DateOnly>("ReleaseDate")
                         .HasColumnType("date");
 
                     b.Property<string>("Title")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<string>("URL")
                         .IsRequired()
                         .HasColumnType("nvarchar(max)");
 
