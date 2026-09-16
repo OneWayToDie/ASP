230fc1690c82143de02df9d7c20fe85b46bb50df
Danila <Danila@DESKTOP-44LP0PV>
2026-09-16 20:26:45 +0300

    lesson 44 TODO: Scaffold Student

---
+++ Blazor/AcademyAgain/Models/Student.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AcademyAgain.Models
+{
+	public class Student:Human
+	{
+		[Key]
+		public int stud_id { get; set; }
+		[Required]
+		public int group { get; set; }
+	}
+}
